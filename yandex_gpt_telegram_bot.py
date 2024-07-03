50fab6c833c45a994c9a10800cd383f39249f22b
Denis Gorbunov <81759161+SkyFallen196@users.noreply.github.com>
2024-07-03 22:55:52 +0500

    Update telegram bots 03.07
    1. The logging module has been added.
    2. Added exception handling in handle_message.
    3. Added Markdown support.

--- yandex_gpt_telegram_bot.py
+++ yandex_gpt_telegram_bot.py
@@ -1,11 +1,17 @@
 import os
+import logging
 from dotenv import load_dotenv
 from telegram import Update, ForceReply
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 from yandex_gpt_python_format import get_response
+from telegram.constants import ChatAction
 
 load_dotenv('.env')
 TELEGRAM_API_KEY = os.getenv('TELEGRAM_API_KEY_YANDEX')
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 async def start(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
@@ -15,9 +21,16 @@ async def start(update: Update, context: CallbackContext) -> None:
     )
 
 async def handle_message(update: Update, context: CallbackContext) -> None:
-    user_message = update.message.text
-    response = get_response(user_message)
-    await update.message.reply_text(response)
+    try:
+        await context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
+        user_message = update.message.text
+        response = get_response(user_message)
+        await update.message.reply_text(response, parse_mode="Markdown")
+
+    except Exception as e:
+        logger.error(f"Exception: {str(e)}")
+        await update.message.reply_text("Извините, произошла ошибка при обработке вашего запроса.")
+
 
 def main() -> None:
     application = Application.builder().token(TELEGRAM_API_KEY).build()
